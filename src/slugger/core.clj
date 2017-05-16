cb26a7e6daf01f4f108b472baadab67799548a08
Dave Liepmann <dave.liepmann@gmail.com>
2017-05-16 16:41:45 +0200

    Move unidecode step to after conversions to fix #5

--- src/slugger/core.clj
+++ src/slugger/core.clj
@@ -6,10 +6,10 @@
   "Convert a UTF-8/16 string into a 7 bit ascii representation suitable for use as a slug in a url."
   [text]
   (-> text
-      c/unidecode
       c/convert-accented-entities
       c/convert-misc-entities
       c/convert-misc-characters
+      c/unidecode
       s/lower-case
       s/trim
       (s/replace #"\s+" "-")))
